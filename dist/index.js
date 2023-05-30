ad07342ab75cf2b9a154b7ea1104bd211c14239e
Vineet Bhoriya <105233380+vineetbhoriya@users.noreply.github.com>
2023-05-30 15:23:04 +0530

    useing typescript

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+let id = 2;
+// varible of number
+let nm = 'vineet';
+// varible of string  
+let adds = [1, 2323, 2323, 212];
+// array of numebr
+// union 
+console.log("name", nm);
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+let id: number = 2;
+// varible of number
+
+let nm :string = 'vineet';
+// varible of string  
+
+let adds: number[] = [1,2323,2323,212] 
+// array of numebr
+
+// union 
+console.log("name",nm)
