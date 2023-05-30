1a39eb703ff018abe3fd8511763496fd9fed1ba6
Vineet Bhoriya <105233380+vineetbhoriya@users.noreply.github.com>
2023-05-30 15:48:22 +0530

    datatypes in typescript

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,35 @@
 "use strict";
 let id = 2;
 // varible of number
-let nm = 'vineet';
-// varible of string  
+let nm = "vineet";
+// varible of string
 let adds = [1, 2323, 2323, 212];
 // array of numebr
-// union 
-console.log("name", nm);
+// union
+let className;
+className = "12";
+className = 12;
+// enum
+var direction1;
+(function (direction1) {
+    direction1[direction1["Up"] = 1] = "Up";
+    direction1[direction1["down"] = 2] = "down";
+    direction1[direction1["left"] = 3] = "left";
+    direction1[direction1["right"] = 4] = "right";
+})(direction1 || (direction1 = {}));
+var direction2;
+(function (direction2) {
+    direction2["Up"] = "up";
+    direction2["down"] = "down";
+    direction2["left"] = "left";
+    direction2["right"] = "right";
+})(direction2 || (direction2 = {}));
+const obj = {
+    id: 1,
+    name: "abc",
+};
+console.log("object id: ", obj.id);
+console.log("object name: ", obj.name);
+// console.log(direction1.Up);
+// console.log(direction1.right);
+// console.log("name", nm);
